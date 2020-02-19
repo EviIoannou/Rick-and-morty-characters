a3b81238e488564584264ec789d15cf6cb34a03c
EviIoannou <evievi@outlook.com>
2020-02-19 15:17:53 +0100

    added introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="intro">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -34,7 +34,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Introduction',
   props: {
     msg: String
   }
